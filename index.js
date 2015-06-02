d7dc833fa9887361f31ffd2f4c35241702a590ca
Brandon Truong <me@brandontruong.com>
2015-06-01 23:35:44 -0500

    all in post function now

--- index.js
+++ index.js
@@ -6,25 +6,9 @@ var express    = require('express'),
 
 app.use(bodyParser.json());
 
-// for slack
 app.post('/', function(req, res){
-	res.send({text: "why is this repeating"});
-	//getHN(res);
-});
-
-// for testing purposes
-app.get('/', function(req, res){
 	getHN(res);
-});
 
-var server = app.listen(process.env.PORT || 3000, function() {
-	var host = server.address().address;
-  var port = server.address().port;
-
-	console.log('Start slacking now at http://%s:%s', host, port);
-});
-
-function getHN(res) {
 	async.waterfall([
 		function(callback) {
 			request.get('https://hacker-news.firebaseio.com/v0/topstories.json', 
@@ -38,30 +22,19 @@ function getHN(res) {
 		function(id, callback){
 			request.get('https://hacker-news.firebaseio.com/v0/item/' + id + '.json',
 				function(err, r, body) {
+
 					// Parse out the valuable information.
 					body = JSON.parse(body);
-
+					
 					if(body.url == "")
 						body.url = "https://news.ycombinator.com/item?id=" + id;
 
-					var message = {}
-
-					message.attachments = [
-						{
-							"fallback": "Something went wrong :(\nWe know you're bored... try the command again maybe?",
-							"pretext": "Here's something to look at while you're bored:",
-							"title": body.title,
-							"title_link": body.url,
-							"text": body.text
-						}
-					]
-
-					message = {
+					var message = {
 						"text": "Slacking test",
 						"attachments": [
 							{
 								"fallback": "Something went wrong :(\nWe know you're bored... try the command again maybe?",
-								"pretext": "Here's something to look at while you're bored:",
+								"pretext": "Bored?",
 								"title": body.title,
 								"title_link": body.url,
 								"text": body.text
@@ -80,4 +53,12 @@ function getHN(res) {
 		else
 			console.log("Relieved boredom.");
 	});
-}
+});
+
+var server = app.listen(process.env.PORT || 3000, function() {
+	var host = server.address().address;
+  var port = server.address().port;
+
+	console.log('Start slacking now at http://%s:%s', host, port);
+});
+
